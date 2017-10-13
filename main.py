0653c435f30e6d8133c2e92dc8bcd5c47eb548da
TheTitoPlay <vocatis.stark@gmail.com>
2017-10-14 01:06:16 +0200

    Update main.py
    changement d'initialisation

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
-''' git@github.com:TheTitoPlay/mdlp-project.git '''
+''' git@github.com:TheTitoPlay/mdlp-project.git |
+    https://github.com/TheTitoPlay/mdlp-project.git'''
 
 import pygame
 
-pygame.init()
+class UntitledGame:
 
-# open new window
-window = pygame.display.set_mode((600, 420), pygame.DOUBLEBUF)
-pygame.display.set_caption('POP CULTURE VOL.1')
-pygame.display.set_icon(pygame.image.load('icon.png'))
+    def __init__(self):
+        ## initialize pygame
+        pygame.init()
+
+        ## open new window
+        window = pygame.display.set_mode((600, 420), pygame.DOUBLEBUF)
+        pygame.display.set_caption('POP CULTURE VOL.1')
+        pygame.display.set_icon(pygame.image.load('icon.png'))
+
+        ## create canvas
+        self._canvas = pygame.Surface((600, 420), pygame.SRCALPHA, 32)
 
 
-done = False
-while done == False:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT :
-            done = True
+        done = False
+        while done == False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT :
+                    done = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        pygame.quit()
     
 
 
