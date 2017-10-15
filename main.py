b61ba27b6cae62f3117fc6d1f95ae42d66fa7cfa
TheTitoPlay <vocatis.stark@gmail.com>
2017-10-15 11:52:28 +0200

    Réorganisation de main.py
    Réécriture pour conventions et lisibilité

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-''' git@github.com:TheTitoPlay/mdlp-project.git |
-    https://github.com/TheTitoPlay/mdlp-project.git'''
+#!/usr/bin/env python
+#
+# Game for MDLP Project
+#
+#git@github.com:TheTitoPlay/mdlp-project.git
+#https://github.com/TheTitoPlay/mdlp-project.git
 
 import pygame
 
 class UntitledGame:
 
     def __init__(self):
-        ## initialize pygame
+        ## initialize screen
         pygame.init()
-
-        ## open new window
-        window = pygame.display.set_mode((600, 420), pygame.DOUBLEBUF)
-        pygame.display.set_caption('POP CULTURE VOL.1')
+        ## window parameters
+        self._window = pygame.display.set_mode((600, 420), pygame.DOUBLEBUF)
+        pygame.display.set_caption('Awsome Untitled Platformer Game')
         pygame.display.set_icon(pygame.image.load('icon.png'))
 
-        ## create canvas
-        self._canvas = pygame.Surface((600, 420), pygame.SRCALPHA, 32)
+        ## create screen
+        self._screen = pygame.Surface(self._window.get_size(), pygame.SRCALPHA, 32)
+        self._screen = self._screen.convert()
+        self._bgcolor = (250, 250, 250)
+        self._screen.fill(self._bgcolor)
+        
+        ## update screen
+        self._window.blit(self._screen, (0, 0))
+        pygame.display.flip()
+        
 
-
+    def run (self):
         done = False
         while done == False:
             for event in pygame.event.get():
@@ -35,6 +46,14 @@ class UntitledGame:
 
 
 
+        ## update screen
+        self._window.blit(self._screen, (0, 0))
+        pygame.display.flip()
 
-
+        
         pygame.quit()
+
+
+if __name__ == '__main__':
+    game = UntitledGame()
+    game.run()
