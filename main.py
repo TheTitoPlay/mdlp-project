de3017da08d4a737985251a1342dee392b37d0e5
TheTitoPlay <vocatis.stark@gmail.com>
2017-10-15 19:06:18 +0200

    Ajout de la sélection de personnage
    Le personnage est selectionnable lors de la création d'une nouvelle
    partie + Ajout d'icônes

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ class UntitledGame:
         ## display/load menu
         self._menu = pygame.image.load('resources\screen\menu.png').convert()
         self._play = pygame.image.load('resources\screen\play.png').convert()
+        self._choose_player = pygame.image.load('resources\screen\choose_player.png').convert()
+        self._player_pick = pygame.image.load('resources\screen\player_pick.png').convert_alpha()
         self._screen.blit(self._menu, (0, 0))
 
         ## update screen        
@@ -53,30 +55,50 @@ class UntitledGame:
                         print(pos)
 
                         ## Play
-                        if 408 < pos[0] < 587 and 250 < pos[1] < 319 and menu_screen == 0:
-                            print("PLAY")
+                        if 416 < pos[0] < 584 and 225 < pos[1] < 286 and menu_screen == 0:
+                            self._screen.blit(self._play, (0, 0))                            
+                            self.update_screen()
                             menu_screen = 1
-                            self._screen.blit(self._play, (0, 0))
-                            self.update_screen()
                         ## Play - New Game
-                        if 383 < pos[0] < 616 and 254 < pos[1] < 308 and menu_screen == 1:
-                            print("New Game")
-                        ## Play - Continue Game
-                        if 347 < pos[0] < 655 and 364 < pos[1] < 415 and menu_screen == 1:
-                            print("Continue Game")
-                        ## Play - Back to Menu
-                        if 341 < pos[0] < 661 and 473 < pos[1] < 526 and menu_screen == 1:
-                            menu_screen = 0
-                            self._screen.blit(self._menu, (0, 0))
+                        if 390 < pos[0] < 613 and 297 < pos[1] < 354 and menu_screen == 1:
+                            menu_screen = 12
+                            self._screen.blit(self._choose_player, (0, 0))
+                            p_position = 0
+                            self._screen.blit(self._player_pick, (457, 337), (p_position,0,80,110))
                             self.update_screen()
-                            
+                        ##Choose Previous Player                        
+                        if 413 < pos[0] < 441 and 385 < pos[1] < 420 and menu_screen == 12 and p_position > 80:
+                            self._screen.blit(self._choose_player, (0, 0))
+                            p_position -= 81
+                            self._screen.blit(self._player_pick, (457, 337), (p_position,0,80,110))
+                            self.update_screen()
+                        ##Choose Next Player
+                        if 552 < pos[0] < 578 and 383 < pos[1] < 424 and menu_screen == 12 and p_position < 324:
+                            self._screen.blit(self._choose_player, (0, 0))
+                            p_position += 81
+                            self._screen.blit(self._player_pick, (457, 337), (p_position,0,80,110))
+                            self.update_screen()
+                        ## Play - Continue Game
+                        if 347 < pos[0] < 655 and 410 < pos[1] < 468 and menu_screen == 1:
+                            print("Continue Game")
+                        
                         ## Settings
-                        if 349 < pos[0] < 648 and 359 < pos[1] < 428 and menu_screen == 0:
+                        if 359 < pos[0] < 646 and 376 < pos[1] < 432 and menu_screen == 0:
                             print("SETTINGS")
                             '''menu_screen = 2'''
                         ## Quit
                         if 425 < pos[0] < 573 and 542 < pos[1] < 591 and menu_screen == 0:
                             done = True
+                ## Play - Return to Menu
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE :
+                        menu_screen = 0
+                        self._screen.blit(self._menu, (0, 0))
+                        self.update_screen()
+
+                    if event.key == pygame.K_RETURN and menu_screen == 12:
+                        print("Jeu lancé avec perso choisi")
+                        
 
 
         ## update screen
